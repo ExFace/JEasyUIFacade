48d1802f405edf7b3b7e2bf27dbc03ff385dfbfc
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2026-09-17 08:56:35 +0200

    FIX JS error when resetting InputDateTime

--- Facades/Elements/EuiInputDateTime.php
+++ Facades/Elements/EuiInputDateTime.php
@@ -18,4 +18,21 @@ class EuiInputDateTime extends EuiInputDate
     {
         return 'datetimebox';
     }
+
+    /**
+     * {@inheritDoc}
+     * @see \exface\JEasyUIFacade\Facades\Elements\EuiInputDate::buildJsValueSetterMethod()
+     */
+    public function buildJsValueSetterMethod($value)
+    {
+        return "each(function() {
+            var jqSelf = $(this);
+            var mValue = {$value};
+            if (mValue == null || mValue === '') {
+                jqSelf.datetimebox('clear');
+            } else {
+                jqSelf.datetimebox('setValue', mValue);
+            }
+        }).trigger('change')";
+    }
 }
